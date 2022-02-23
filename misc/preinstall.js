7100e1976acec5e663a1dd53d378d3b45b311130
trickeyone <joshua.en@gmail.com>
2022-02-23 01:04:52 -0600

    chore: moved back to NPM v7

--- misc/preinstall.js
+++ misc/preinstall.js
@@ -1,13 +1,13 @@
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
+if (process.env.npm_execpath.indexOf('npm') === -1) {
   throw new Error(`
-  
-  ###################################################
-  #                                                 #
-  #  Please use Yarn > 1.3 to install dependencies  #
-  #                                                 #
-  #  1. Install yarn (https://yarnpkg.com/)         #
-  #  2. Run 'yarn' instead of 'npm install'         #
-  #                                                 #
-  ###################################################
+
+  ####################################################
+  #                                                  #
+  #  Please use Npm > 6.5.0 to install dependencies  #
+  #                                                  #
+  #  1. Install npm                                  #
+  #  2. Run 'npm install'                            #
+  #                                                  #
+  ####################################################
   `);
 }
