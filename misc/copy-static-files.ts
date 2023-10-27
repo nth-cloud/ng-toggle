50220fa95454cfcd866a014ee5df5047cb14cb60
trickeyone <joshua.en@gmail.com>
2023-10-27 00:47:18 -0500

    build: added schematics to static file build

--- misc/copy-static-files.ts
+++ misc/copy-static-files.ts
@@ -5,9 +5,15 @@ import { copyFileSync, copySync, readJsonSync, writeJSONSync } from 'fs-extra';
  */
 
 const DEST = 'dist';
+const SCHEMATICS = `${DEST}/schematics`;
 
 // 1. Copy static assets
 ['LICENSE', 'README.md'].forEach((file) => copyFileSync(file, `${DEST}/${file}`));
+
+// 2. Copy built schematics to 'dist/ng-bootstrap'
+copySync('schematics/dist', SCHEMATICS);
+copyFileSync('schematics/collection.json', `${SCHEMATICS}/collection.json`);
+copyFileSync('schematics/ng-add/schema.json', `${SCHEMATICS}/ng-add/schema.json`);
 
 // 3. Patch version in the 'dist/package.json' with the one from 'package.json'
 const {
